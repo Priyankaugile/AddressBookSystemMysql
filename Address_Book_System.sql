8483aed4bdbccafc7b93d7351ecf57e36a8e176b
priyankaugile <ugilepriyanka26@gmail.com>
2021-09-29 17:39:29 +0530

    Added: Creating new database.

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -1,2 +1,10 @@
 ***********************************************************
       ********"Welcome to mysql database"********
+
+#UC1 - Creating Database
+
+mysql> CREATE DATABASE AddressBookService;
+Query OK, 1 row affected (0.76 sec)
+
+mysql> use AddressBookService
+Database changed
